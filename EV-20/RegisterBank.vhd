498006ed3010cd2b1babf8bd904858682e452305
Javier Kipen <javier1kipen@gmail.com>
2020-05-20 13:10:59 -0300

    Register bank competo sin validar
    Se agregó la parte de manejo de memoria desde register bank. Fata hacer un testbench para probarlo.
    Además, se agrego el bloque a quartus y el bloque de memoria desde data

--- EV-20/RegisterBank.vhd
+++ EV-20/RegisterBank.vhd
@@ -11,6 +11,8 @@ ENTITY RegisterBank IS
 		C		:  IN  STD_LOGIC_VECTOR(5 downto 0);
 		From_C  :  IN  STD_LOGIC_VECTOR(15 downto 0);
 		sys_clk	:  IN  STD_LOGIC;
+		M_read	:  IN  STD_LOGIC; -- 1 cuando se trae de memoria al registro
+		To_W	:  IN  STD_LOGIC_VECTOR(15 downto 0); -- El dato de memoria que trae
 		To_A  	:  OUT  STD_LOGIC_VECTOR(15 downto 0);
 		To_B  	:  OUT  STD_LOGIC_VECTOR(15 downto 0);
 		W    	:  OUT STD_LOGIC_VECTOR(15 downto 0)
@@ -27,7 +29,14 @@ begin
     begin
         if(rising_edge(sys_clk)) then
 			if( to_integer(unsigned(C)) < 35 ) then
-				reg_map(to_integer(unsigned(C))) <= From_C;
+				if (M_read = '1') then
+					reg_map(34) <= To_W;
+				else
+					reg_map(to_integer(unsigned(C))) <= From_C;
+				end if;
+				if((M_read = '1') and (to_integer(unsigned(C)) /= 34)) then --Esto hay que verificarlo: Para escribir desde memoria y c al mismo tiempo a registros
+					reg_map(to_integer(unsigned(C))) <= From_C;
+				end if;
 			end if;	
 			To_A <= reg_map(to_integer(unsigned(A)));
 			To_B <= reg_map(to_integer(unsigned(B)));
