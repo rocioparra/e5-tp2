3eb238d3c3cd897ed0c734211dde38d103fa8e81
Javier Kipen <javier1kipen@gmail.com>
2020-05-25 12:45:18 -0300

    Se hizo uc2 como estaba en la hoja de gonza
    Falta probarlo

--- EV - 20/uc2.vhd
+++ EV - 20/uc2.vhd
@@ -16,10 +16,26 @@ ENTITY uc2 IS
 		T5				:  IN STD_LOGIC_VECTOR(6 downto 0);
 		hold  		:  OUT  STD_LOGIC
     );
+	
+	
 END uc2;
 
-ARCHITECTURE behaviour OF uc2 IS    
+ARCHITECTURE behaviour OF uc2 IS 
+	constant WR : integer := 0;
+	constant WW : integer := 1;
+	constant RR : integer := 2;
+	constant RW : integer := 3;
+	constant CR : integer := 4;
+	constant CW : integer := 5;
+	constant J  : integer := 6;
 begin
-    hold <= '0';
+    hold <= '1' when((T3(CR) and (T4(CW) or T5(CW)))='1'	or	
+	
+					 (T3(WR) and (T4(WW) or T5(WW)))='1' 	or
+					 
+					 (T3(RR)='1' and T4(RW)='1' and ((A2=C4(4 downto 0)) or (B2=C4)) ) 	or
+					 
+					 (T3(RR)='1' and T5(RW)='1' and ((A2=C5(4 downto 0)) or (B2=C5)) ) ) 		else 	
+		    '0';
 
 END behaviour;
