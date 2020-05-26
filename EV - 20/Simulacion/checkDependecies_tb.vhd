35a0dfdaebc611653f180ae8fb41d76918b44e6e
Javier Kipen <javier1kipen@gmail.com>
2020-05-26 16:19:48 -0300

    Se corrigió uc1 para la simulación
    Se probó la simulación para ver el hold, pero hay un problema que no converge la misma.

--- EV - 20/Simulacion/checkDependecies_tb.vhd
+++ EV - 20/Simulacion/checkDependecies_tb.vhd
@@ -2,11 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity uIHandlerROM_tb is
-end entity uIHandlerROM_tb;
+entity checkDeps_tb is
+end entity checkDeps_tb;
 
 
-architecture testbench of uIHandlerROM_tb is
+architecture testbench of checkDeps_tb is
 
 	component uIHandler
 	Port 
@@ -47,10 +47,11 @@ architecture testbench of uIHandlerROM_tb is
 	--signal DAdd : STD_LOGIC_VECTOR(9 DOWNTO 0);
 	--signal sh : STD_LOGIC_VECTOR(1 DOWNTO 0);
 	
-	constant N_instr : integer := 5;
+	constant N_instr : integer := 4;
 	type t_instArray is array (0 to N_instr) of std_logic_vector(13 downto 0);
 	signal instArray : t_instArray;
 	signal simulationEnded 	: boolean := false;
+	signal instr_i 	: integer range 0 to N_instr :=0;
 
 begin
 	instArray <=(("00010011111111"),  -- MOV W,#0xFF
@@ -60,28 +61,24 @@ begin
 				 ("00011101000100"));  -- ADK W,0x88
 	memoria: uIROM port map (clock => sys_clk, q=>ui_mem, address=>instruction_in(13 downto 7));
 	uicontrol: uIHandler port map (sys_clk => sys_clk, Instruction=>instruction_in, uI_mem=>ui_mem,hold=>hold);
-   -- Clock process definitions
+      -- Clock process definitions
 	clock_process :process
 	begin
 		sys_clk <= '0';
-		wait for 1 ns;
+		wait for 10 ps;
 		sys_clk <= '1';
-		wait for 1 ns;
+		wait for 10 ps;
 		if simulationEnded then
 			wait;
 		end if;
 	end process;
-
-	stim_proc: process
+	instruction_in <= instArray(instr_i);
+	stim_proc: process(sys_clk)
 	begin      
-		instruction_in <= "00010011111111";
-		for i in 0 to N_instr loop
-			wait until falling_edge(sys_clk);
-			wait until (hold= '0');
-			instruction_in <= instArray(i);
-			end loop;
-		end loop;
-		simulationEnded <= true;
-   		wait;
+		if(falling_edge(sys_clk)) then
+			--if(hold = '0') then
+				instr_i <=instr_i + 1;
+			--end if;
+		end if;
 	end process;
 end testbench;
