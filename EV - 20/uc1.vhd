79de744a2aa5860b09fe9d709f9fe821d8ecc949
Javier Kipen <javier1kipen@gmail.com>
2020-06-04 02:01:09 -0300

    Se agregó lo de la alu y bloque uc2
    Se puso para que la alu haga nop cuando tenga una operación desconocida, que el bloque uc1 bloquee alu en hold, la lógica de uc2 para jumps y  la nueva salida de uc2

--- EV - 20/uc1.vhd
+++ EV - 20/uc1.vhd
@@ -5,6 +5,7 @@ use ieee.numeric_std.all;   -- Needed for shifts
 ENTITY uc1 IS 
     PORT
     (
+		ALU_in	:  IN STD_LOGIC_VECTOR(3 downto 0);
 		M			:  IN STD_LOGIC_VECTOR(1 downto 0);
 		B_in		:  IN STD_LOGIC_VECTOR(5 downto 0);
 		T_in		:  IN STD_LOGIC_VECTOR(6 downto 0);
@@ -13,6 +14,7 @@ ENTITY uc1 IS
 		sys_clk  :  IN  STD_LOGIC;
 		MW			:  OUT  STD_LOGIC;
 		MR			:  OUT  STD_LOGIC;
+		ALU_Out	:  OUT STD_LOGIC_VECTOR(3 downto 0);
 		T			:  OUT STD_LOGIC_VECTOR(6 downto 0);
 		C			:  OUT STD_LOGIC_VECTOR(5 downto 0)
     );
@@ -23,6 +25,7 @@ begin
 	
 	T <= T_in when hold ='0' else "0000000";
 	C <= C_in when hold ='0' else "100011";
+	ALU_out <= ALU_in when hold ='0' else "1111"; --NOP
 	MW <= M(0) when hold = '0' else '0'; 
 	MR <= M(1) when hold = '0' else '0';
 	
