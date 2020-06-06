0f7f3f766c220ff129e79d30aea2fc725c17e374
Javier Kipen <javier1kipen@gmail.com>
2020-06-06 18:04:06 -0300

    Parece andar el salto condicional
    Se arregló un problema con los carries en la ALU

--- EV - 20/ALU.vhd
+++ EV - 20/ALU.vhd
@@ -20,20 +20,19 @@ ARCHITECTURE behaviour OF ALU IS
 begin
     output    <= out_aux(15 downto 0); 
 	cy_out 	  <= out_aux(16); 
-	out_aux <= 	'0' & a 						when ctrl = "0000" else
-				'0' & b 						when ctrl = "0001" else 
-				'0' & (not a) 					when ctrl = "0010" else 
-				'0' & (not b) 					when ctrl = "0011" else 
+	out_aux <= 	cy_in & a 						when ctrl = "0000" else
+				cy_in & b 						when ctrl = "0001" else 
+				cy_in & (not a) 					when ctrl = "0010" else 
+				cy_in & (not b) 					when ctrl = "0011" else 
 				('0' & a) + ('0' & b) 			when ctrl = "0100" else 
 				('0' & a) + ('0' & b) + cy_in 	when ctrl = "0101" else 
-				'0' & (a or b) 					when ctrl = "0110" else 
-				'0' & (a and b) 				when ctrl = "0111" else 
-				"00000000000000000" 			when ctrl = "1000" else 
-				"00000000000000001"  			when ctrl = "1001" else 
-				"01111111111111111" 	 		when ctrl = "1010" else 
+				cy_in & (a or b) 					when ctrl = "0110" else 
+				cy_in & (a and b) 				when ctrl = "0111" else 
+				cy_in &"0000000000000000" 			when ctrl = "1000" else 
+				cy_in &"0000000000000001"  			when ctrl = "1001" else 
+				cy_in &"1111111111111111" 	 		when ctrl = "1010" else 
 				"00000000000000000"  			when ctrl = "1011" else 
 				"10000000000000000"  			when ctrl = "1100" else 
-				"10000000000000000"  			when (unsigned(ctrl) > 12) and cy_in='1' else 
-				"00000000000000000";
+				cy_in &"0000000000000000";
 
 END behaviour;
